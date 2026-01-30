f399430aff540f1b8fa27caac3d24158addc5b3a
anta seck <antalissa10@gmail.com>
2026-01-30 15:35:50 +0000

    api

--- src/api.js
+++ src/api.js
@@ -1,1 +1,35 @@
-const API_URL = "https://red-backend-neww-production.up.railway.app/api";
+// export const API_URL = import.meta.env.VITE_API_URL;
+
+
+export const API_URL =
+  "https://red-backend-neww-production.up.railway.app/api";
+
+export const registerUser = async (userData) => {
+  const response = await fetch(`${API_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+
+  const data = await response.json();
+  if (!response.ok) throw data;
+  return data;
+};
+
+export const loginUser = async (userData) => {
+  const response = await fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+
+  const data = await response.json();
+  if (!response.ok) throw data;
+  return data;
+};
