55e438d3fc98d43b8f0cf6b46079ae98cfbd822e
anta seck <antalissa10@gmail.com>
2026-02-01 01:39:27 +0000

    add vercel-json

--- src/pages/dashboard/ListHotels.jsx
+++ src/pages/dashboard/ListHotels.jsx
@@ -174,9 +174,7 @@
 import { useState, useEffect } from "react";
 import { FaPlus, FaCamera } from "react-icons/fa";
 
-// const API_URL = import.meta.env.VITE_API_URL; 
-// Mauvais
-const API_URL = "https://red-backend-neww.up.railway.app/api";
+const API_URL = import.meta.env.VITE_API_URL; 
 
 const ListHotels = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
