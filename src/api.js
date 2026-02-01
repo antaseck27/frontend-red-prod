3c6b85397f9ab0256ffa9a880d91799b74397fa5
anta seck <antalissa10@gmail.com>
2026-02-01 01:15:07 +0000

    add vercel-json

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 // export const API_URL = import.meta.env.VITE_API_URL;
 
 
-export const API_URL =
-  "https://red-backend-neww-production.up.railway.app/api";
-
+// export const API_URL =
+//   "https://red-backend-neww-production.up.railway.app/api"; 
+export const API_URL = "https://red-backend-neww.up.railway.app/api";
 export const registerUser = async (userData) => {
   const response = await fetch(`${API_URL}/register`, {
     method: "POST",
